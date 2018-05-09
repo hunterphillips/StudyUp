7eeedd261528fcc1c50eeb53c62932447f6995fb
Hunter Phillips <hkphillips42@gmail.com>
2018-05-08 21:18:38 -0500

    quiz score, answer highlight, back() directive, quiz styles

--- client/app.js
+++ client/app.js
@@ -1,26 +1,43 @@
 'use strict';
 // import 'bulma/css/bulma.css';
-angular.module('StudyU', ['ngRoute', 'ngAnimate']).config($routeProvider => {
-  //
-  $routeProvider
-    .when('/', {
-      templateUrl: 'partials/auth-form.html',
-      controller: 'AuthCtrl'
-    })
-    .when('/home', {
-      templateUrl: 'partials/home.html',
-      controller: 'HomeCtrl'
-    })
-    .when('/course/:id', {
-      templateUrl: 'partials/course.html',
-      controller: 'CourseCtrl'
-    })
-    .when('/quiz/:id', {
-      templateUrl: 'partials/quiz.html',
-      controller: 'QuizCtrl'
-    })
-    .otherwise('/');
-});
+angular
+  .module('StudyU', ['ngRoute', 'ngAnimate'])
+  .config($routeProvider => {
+    //
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/auth-form.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/course/:id', {
+        templateUrl: 'partials/course.html',
+        controller: 'CourseCtrl'
+      })
+      .when('/quiz/:id', {
+        templateUrl: 'partials/quiz.html',
+        controller: 'QuizCtrl'
+      })
+      .otherwise('/');
+  })
+
+  // Back Button Directive
+  .directive('back', [
+    '$window',
+    function($window) {
+      return {
+        restrict: 'A',
+        link: function(scope, elem, attrs) {
+          elem.bind('click', function() {
+            $window.history.back();
+          });
+        }
+      };
+    }
+  ]);
 
 // TODO:   **** controller
 // On page refresh, the currentUser set in the auth factory is lost, bc its a variable. We need to ask the backend for the user it has stored in session so we can reestablish current user. Below, we listen for a route change and call a method that will ping the backend for the logged-in user, then broadcast that information via a custom event to the listening controllers
--- client/controllers/QuizCtrl.js
+++ client/controllers/QuizCtrl.js
@@ -1,53 +1,66 @@
 'use strict';
-angular.module('StudyU').controller('QuizCtrl', function(
-  $scope,
-  QuizFactory,
-  AuthFactory,
-  $routeParams,
-  $timeout
-  // $location
-) {
-  //initialize
-  $scope.questions = [];
-  $scope.currentQuestion = 0;
+angular
+  .module('StudyU')
+  .controller('QuizCtrl', function(
+    $scope,
+    QuizFactory,
+    AuthFactory,
+    $routeParams,
+    $rootScope,
+    $timeout,
+    $location
+  ) {
+    //initialize
+    $scope.questions = [];
+    $scope.currentQuestion = 0;
+    //track quiz results
+    $scope.score = 0;
 
-  $scope.$on('handleBroadcast', function(event, currentUser) {
-    $scope.user = currentUser;
-  });
+    $scope.$on('handleBroadcast', function(event, currentUser) {
+      $scope.user = currentUser;
+    });
 
-  $scope.$on('$viewContentLoaded', function() {
-    QuizFactory.getQuizQuestions(+$routeParams.id).then(data => {
-      $scope.questions = data;
-      // apply default 'selected' value to each group of question answers
-      $scope.questions.forEach(question => {
-        $scope[`selected${question.id}`] = false; // ng-disabled in template
+    $scope.$on('$viewContentLoaded', function() {
+      QuizFactory.getQuizQuestions(+$routeParams.id).then(data => {
+        $scope.questions = data;
+        // apply default 'selected' value to each group of question answers
+        $scope.questions.forEach(question => {
+          $scope[`selected${question.id}`] = false; // ng-disabled in template
+        });
       });
     });
+
+    // update 'selected' value for this question's answers
+    $scope.select = function() {
+      this.selected = true;
+      $scope[`selected${this.answer.question_id}`] = true;
+      // compare answer's id to question's answer_id and pass to updateScore
+      updateUserScore(this.answer.id === this.$parent.question.answer_id);
+      nextQuestion();
+    };
+
+    const updateUserScore = result => {
+      result = result === true ? 1 : 0;
+      $scope.score += result;
+      QuizFactory.submitAnswer({ user_id: $scope.user.id, result: result });
+    };
+
+    const nextQuestion = () => {
+      $timeout(function() {
+        $scope.currentQuestion += 1;
+        if ($scope.currentQuestion === $scope.questions.length) {
+          // convert quiz score to %
+          $scope.score = $scope.score / $scope.questions.length * 100;
+          $scope.completed = true;
+        }
+      }, 1800);
+    };
+
+    // TODO: loading gif
+    // $scope.setDelay = function() {
+    //   $scope.delay = true;
+    //   $timeout(function() {
+    //     $scope.delay = false;
+    //   }, 1000);
+    // };
   });
-
-  // update 'selected' value for this question's answers
-  $scope.select = function() {
-    $scope[`selected${this.answer.question_id}`] = true;
-    // compare answer's id to question's answer_id and pass to updateScore
-    updateUserScore(this.answer.id === this.$parent.question.answer_id);
-    nextQuestion();
-  };
-
-  const updateUserScore = result => {
-    result = result === true ? 10 : -5;
-    QuizFactory.submitAnswer({ user_id: $scope.user.id, result: result });
-  };
-
-  const nextQuestion = () => {
-    $timeout(function() {
-      $scope.currentQuestion += 1;
-    }, 1800);
-  };
-
-  // $scope.setDelay = function() {
-  //   $scope.delay = true;
-  //   $timeout(function() {
-  //     $scope.delay = false;
-  //   }, 1000);
-  // };
-});
